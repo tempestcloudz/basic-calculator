363427003bf2238e423e532335875dd0d9c34f04
cloudz_code <163407680+tempestcloudz@users.noreply.github.com>
2025-05-05 20:25:08 +0800

    Add files via upload

---
+++ decent/src/calc_classes/Side.java
@@ -0,0 +1,35 @@
+/* package decent;
+
+ import java.util.Date;
+
+public class Side {
+        public static void main(String[] arg) {
+            byte age = 30;
+            Date now = new Date();
+            System.out.println(now);
+        }
+
+    }
+
+
+
+public class Side {
+    static String name = "Alex";
+    static Integer visit = 3;
+    static String suffix = "st";
+        public static void main(String[] args) {
+            if(visit == 2) {
+                suffix = "nd";
+            }
+            if(visit == 3) {
+                suffix = "rd";
+            }
+            if(visit == 4) {
+                suffix = "th";
+            }
+            System.out.println("Hello, " + name + "! You are the " + visit + suffix + " visitor!");
+
+
+    }
+}
+*/
